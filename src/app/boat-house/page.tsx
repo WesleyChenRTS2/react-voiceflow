00d58280928e8de5528b02bdc9068eec7e50de75
Wesley Chen <121049060+WesleyChenRTS2@users.noreply.github.com>
2024-02-09 14:01:06 -0500

    added 'what can i ask' seciton

--- src/app/boat-house/page.tsx
+++ src/app/boat-house/page.tsx
@@ -11,6 +11,24 @@ const BoatHouse = () => {
     >
       <ChatbotLoader />
       <JsonDisplay data={PropertyPayload} />
+      <div className="max-w-4xl mx-auto py-10">
+        <h1 className="text-xl font-semibold mb-4">
+          Questions You Can Ask the Bot
+        </h1>
+        <ul className="list-disc space-y-2 pl-5">
+          <li className="text-md">Make reservation</li>
+          <li className="text-md">Give feedback</li>
+          <li className="text-md">Get weather</li>
+          <li className="text-md">
+            Ask about Twiddy vacation rental FAQ questions, for example:
+          </li>
+          <ul className="list-disc space-y-1 pl-8">
+            <li className="text-md">Cancellation policy</li>
+            <li className="text-md">Check-in & checkout time</li>
+            <li className="text-md">Things to do in OBX</li>
+          </ul>
+        </ul>
+      </div>
     </div>
   );
 };
