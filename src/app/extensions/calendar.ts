e35c09dd55fe17bc774b419828ad0208672e6c61
Matteo Ricci <matteo.a.ricci@gmail.com>
2024-07-03 14:46:48 -0400

    made changes to what can be selected

--- src/app/extensions/calendar.ts
+++ src/app/extensions/calendar.ts
@@ -1,5 +1,9 @@
 import { CalendarPayload } from "./types";
 
+interface DayMap {
+  [key: string]: number;
+}
+
 const CalendarExtension = {
   name: "Calendar",
   type: "response",
@@ -9,6 +13,21 @@ const CalendarExtension = {
 
     const styles = document.createElement("style");
     styles.innerHTML = `
+
+    .ranges {
+      color: #10ac74;
+      font-weight: bold;
+    }
+    .ranges.selected, .ranges.selected:hover {
+      background-color: #10ac74;
+      font-weight: bold;
+      color: white;
+    }
+      .selected {
+        background-color: rgb(242, 255, 250);
+        color: black;
+      }
+
     .btn-container {
         display: flex;
         flex-direction: column;
@@ -38,6 +57,13 @@ const CalendarExtension = {
       color: #2E7FF1;
     }`;
 
+    const dayMap: DayMap = {
+      Sunday: 0,
+      Saturday: 6,
+      Friday: 5,
+      Thursday: 4,
+    };
+
     calendarContainer.append(styles);
 
     const calendarDiv = document.createElement("div");
@@ -63,53 +89,100 @@ const CalendarExtension = {
         });
         return dateDisabled;
       },
-      maxNumberOfDates: trace.payload.disableButtons ? 0 : 2,
-      defaultViewDate: trace.payload.startDate && trace.payload.startDate !== "null" ? trace.payload.startDate.replaceAll("-", "/") : new Date()
+      maxNumberOfDates: trace.payload.disableButtons ? 0 : 7,
+      defaultViewDate:
+        trace.payload.startDate && trace.payload.startDate !== "null"
+          ? trace.payload.startDate.replaceAll("-", "/")
+          : new Date(),
+      // @ts-ignore
+      beforeShowDay: function (date) {
+        if (date.getDay() === dayMap[trace.payload.checkInDay || "noDate"]) {
+          return {
+            classes: "ranges",
+          };
+        }
+      },
     });
 
     element.append(calendarContainer);
 
     if (!trace.payload.disableButtons) {
-      const btnContainer = document.createElement("div");
-      btnContainer.className = "btn-container";
+    const btnContainer = document.createElement("div");
+    btnContainer.className = "btn-container";
 
-      const submitBtn = document.createElement("button");
-      submitBtn.innerText = "Submit";
-      submitBtn.className = "submit";
-      submitBtn.disabled = true;
+    const submitBtn = document.createElement("button");
+    submitBtn.innerText = "Submit";
+    submitBtn.className = "submit";
+    submitBtn.disabled = true;
 
-      const cancelBtn = document.createElement("button");
-      cancelBtn.innerText = "Cancel";
-      cancelBtn.className = "cancel";
+    const cancelBtn = document.createElement("button");
+    cancelBtn.innerText = "Cancel";
+    cancelBtn.className = "cancel";
 
-      btnContainer.append(submitBtn, cancelBtn);
+    btnContainer.append(submitBtn, cancelBtn);
 
-      element.append(btnContainer);
+    element.append(btnContainer);
 
-      submitBtn.addEventListener("click", function () {
-        submitBtn.remove();
-        cancelBtn.remove();
-        window.voiceflow.chat.interact({
-          type: "Response_Submitted",
-          payload: { dates: datepicker.getDate() },
-        });
+    submitBtn.addEventListener("click", function () {
+      submitBtn.remove();
+      cancelBtn.remove();
+      window.voiceflow.chat.interact({
+        type: "Response_Submitted",
+        payload: {
+          dates: [
+            datepicker.getDate()[0],
+            datepicker.getDate()[datepicker.getDate().length - 1],
+          ],
+        },
       });
+    });
 
-      cancelBtn.addEventListener("click", function () {
-        submitBtn.remove();
-        cancelBtn.remove();
-        window.voiceflow.chat.interact({
-          type: "Cancel",
-        });
+    cancelBtn.addEventListener("click", function () {
+      submitBtn.remove();
+      cancelBtn.remove();
+      window.voiceflow.chat.interact({
+        type: "Cancel",
       });
+    });
 
-      datepicker.element.addEventListener("changeDate", () => {
-        if (datepicker.getDate().length !== 2) {
-          submitBtn.disabled = true;
-        } else {
-          submitBtn.disabled = false;
-        }
+    //@ts-ignore
+    const getDates = function (startDate, endDate) {
+      const dates = [];
+      let currentDate = startDate;
+      //@ts-ignore
+      const addDays = function (days) {
+        //@ts-ignore
+        const date = new Date(this.valueOf());
+        date.setDate(date.getDate() + days);
+        return date;
+      };
+      while (currentDate <= endDate) {
+        dates.push(currentDate);
+        currentDate = addDays.call(currentDate, 1);
+      }
+      return dates;
+    };
+    //@ts-ignore
+    datepicker.element.addEventListener("changeDate", (e) => {
+      const dateRange = trace.payload.dates?.find((d) => {
+        return (
+          e.detail.viewDate.getTime() >= new Date(d.start).getTime() &&
+          e.detail.viewDate.getTime() <= new Date(d.end)
+        );
       });
+      const dates = getDates(
+        //@ts-ignore
+        new Date(dateRange?.start),
+        //@ts-ignore
+        new Date(dateRange?.end)
+      );
+      datepicker.setDate(dates, { clear: true });
+      if (datepicker.getDate().length === 0) {
+        submitBtn.disabled = true;
+      } else {
+        submitBtn.disabled = false;
+      }
+    });
     }
   },
 };
